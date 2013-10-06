06d44aa2d2c39b9bfb908962cdfd304a059e6a23
Eliot Arntz <arntzel@gmail.com>
2013-10-06 19:16:43 +0200

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Eliot Arntz on 10/6/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Eliot Arntz on 10/6/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
